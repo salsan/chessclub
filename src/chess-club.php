024a5a6a8cce1ad6222d9f3daf21a45f06cb4574
Salvatore Santagati <github@salsan.dev>
2023-10-02 16:28:05 +0200

    feat: added initial file for plugin

---
+++ src/chess-club.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name:       Chess Club
+ * Plugin URI:        https://github.com/salsan/Chess-Club
+ * Description:       Chess Club Plugin for WordPress: Effortlessly Integrate Your Chess Club's Data into Your WordPress Website.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      8.1
+ * Author:            Salvatore Santagati
+ * Author URI:        https://salsan.dev/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       chess-club
+ * Domain Path:       /languages
+ *
+ * @package Chess-Club
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;   // Exit if accessed directly!
+}
