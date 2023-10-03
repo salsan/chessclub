70ff87d423498e1a9db6dd5486133162eac11ca6
Salvatore Santagati <github@salsan.dev>
2023-10-03 12:10:29 +0200

    chore: added proxy server browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,36 @@
 const { src, dest, series, watch } = require("gulp");
 const server = require("browser-sync").create();
 
-function serve(cb) {
-  server.init({
-    browser: "chrome",
 
-    proxy: {
-      target: "http://localhost:8888",
-    },
-  });
-  cb();
+function serve(cb) {
+	server.init(
+		{
+			browser: "chrome",
+
+			proxy: {
+				target: "http://localhost:8888",
+			},
+		}
+	);
+	cb();
 }
 
 function reload(cb) {
-  server.reload();
-  cb();
+	server.reload();
+	cb();
 }
 
 function copyTask() {
-  return src(["src/**/*.php", "!src/vendor/**/{tests,squizlabs}/**"]).pipe(
-    dest("dist")
-  );
+	return src(["src/**/*.php", "!src/vendor/**/{tests,squizlabs,phpcsstandards,dealerdirect,wp-coding-standards}/**"]).pipe(
+		dest("dist")
+	);
 }
 
 function watchTask() {
-  watch(["src/**/*.php"], series(copyTask, reload));
+	watch(["src/**/*.php"], series(copyTask, reload));
 }
 
 exports.default = series(copyTask, serve, watchTask);
 exports.copyTask = copyTask;
 exports.serve = serve;
+exports.watchTask = watchTask;
