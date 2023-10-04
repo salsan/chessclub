621187fb5ec203e21301be83ba7efb9ed40967b5
Salvatore Santagati <me@salsan.dev>
2023-10-04 10:32:17 +0200

    feat: added initial support shortcode

--- src/chess-club.php
+++ src/chess-club.php
@@ -13,9 +13,60 @@
  * Text Domain:       chess-club
  * Domain Path:       /languages
  *
- * @package Chess-Club
+ * @package ChessClub
  */
+
+namespace chessclub;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;   // Exit if accessed directly!
 }
+
+if ( file_exists( realpath( __DIR__ . '/vendor/autoload.php' ) ) ) {
+	require_once realpath( __DIR__ . '/vendor/autoload.php' );
+}
+
+use Salsan\Members;
+
+/**
+ * Return total number of members of club for year.
+ *
+ * @param array $atts Array contain value for query.
+ *
+ *  $params = [
+ *      'id'   => (integer) id chess club on federation. Required.
+ *      'anno' => (date)  select year of subscription. Required.
+ *      'type' => ('total', 'rookie') all is total members of club, rookie count only new member. Required.
+ *
+ * @return int
+ */
+function shortcode_cc_number_members( $atts ) {
+
+	$params = shortcode_atts(
+		array(
+			'id'   => '',
+			'year' => '',
+			'type' => '',
+		),
+		$atts
+	);
+
+	$query = new Members\Query(
+		array(
+			'clubId'         => $params['id'],
+			'membershipYear' => $params['year'],
+		)
+	);
+
+	return sanitize_text_field( $query->getNumber()[ $params['type'] ] );
+}
+
+/**
+ *  Inizialization shortcode
+ *
+ * @return void  */
+function init_shortcode() {
+	add_shortcode( 'cc_number_members', 'chessclub\shortcode_cc_number_members' );
+}
+
+add_action( 'init', 'chessclub\init_shortcode' );
