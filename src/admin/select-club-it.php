d6b73d14580eb6033441d5438032397a838f8362
salsan <me@salsan.dev>
2023-11-10 12:37:02 +0100

    refactor: changed code from php to html

--- src/admin/select-club-it.php
+++ src/admin/select-club-it.php
@@ -1,10 +1,7 @@
 <?php // phpcs:disable Generic.Commenting.MissingFileComment
 declare(strict_types=1);
-
-echo '<select id="chessclub_settings" name="chessclub_settings">';
-		echo '<option value="">Select Club</option>';
-
-		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-		echo \Salsan\Chessclub\Includes\Chess\Html::form_option_club_it();
-
-echo '</select>';
+?>
+<select id="chessclub_settings" name="chessclub_settings">
+	<option value="">Select Club</option>
+	<?php echo \Salsan\Chessclub\Includes\Chess\Html::form_option_club_it(); ?>
+</select>
--- src/admin/select-federation.php
+++ src/admin/select-federation.php
@@ -1,10 +1,7 @@
 <?php // phpcs:disable Generic.Commenting.MissingFileComment
 declare(strict_types=1);
-
-echo '<select id="chessclub_settings" name="chessclub_settings" class="federation">';
-		echo '<option value="">Select Federation</option>';
-
-		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-		echo \Salsan\Chessclub\Includes\Chess\Html::form_option_federation();
-
-echo '</select>';
+?>
+<select id="chessclub_settings" name="chessclub_settings" class="federation">
+	<option value="">Select Federation</option>
+	<?php echo \Salsan\Chessclub\Includes\Chess\Html::form_option_federation(); ?>
+</select>
