9934cb09070bbd9467ae7f30dd85ba3c4fbf860f
salsan <me@salsan.dev>
2023-11-11 12:14:35 +0100

    feat: initial support for more options

--- src/admin/select-club-it.php
+++ src/admin/select-club-it.php
@@ -1,7 +1,7 @@
 <?php // phpcs:disable Generic.Commenting.MissingFileComment
 declare(strict_types=1);
 ?>
-<select id="chessclub_settings" name="chessclub_settings">
+<select id="chessclub_settings" name="chessclub_settings[id]">
 	<option value="">Select Club</option>
 	<?php echo \Salsan\Chessclub\Includes\Chess\Html::form_option_club_it(); ?>
 </select>
--- src/admin/select-federation.php
+++ src/admin/select-federation.php
@@ -1,7 +1,7 @@
 <?php // phpcs:disable Generic.Commenting.MissingFileComment
 declare(strict_types=1);
 ?>
-<select id="chessclub_settings" name="chessclub_settings" class="federation">
+<select id="chessclub_settings" name="chessclub_settings[id]" class="federation">
 	<option value="">Select Federation</option>
 	<?php echo \Salsan\Chessclub\Includes\Chess\Html::form_option_federation(); ?>
 </select>
